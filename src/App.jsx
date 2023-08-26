743c3915c0f9e8da3b4f8c94a9ace56a072265f1
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-08-26 19:54:30 +0330

    implementing main pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-function App() {
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import HomePage from "./pages/HomePage";
+import PageNotFound from "./pages/PageNotFound";
 
+function App() {
     return (
-        <div>
-            World wise
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="product" element={<Product />} />
+                <Route path="pricing" element={<Pricing />} />
+                {/* when we pass * as the path of the route , if the user enter any path in url that doesnt exist in our routes , then he will redirect into the element that we pass to this route  */}
+                <Route path="*" element={<PageNotFound />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
 
