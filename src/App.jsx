cbc46b36f8aba7d0eb875b7838bb909680494d22
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-08-27 14:22:35 +0330

    redirect to app cities

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import HomePage from "./pages/HomePage";
@@ -47,8 +47,7 @@ function App() {
                     <Route path="pricing" element={<Pricing />} />
                     <Route path="login" element={<Login />} />
                     <Route path="app" element={<AppLayout />}>
-                        {/* when we add the index property to a child route , it will become the default route that will render if none of the other child routes render  */}
-                        <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
+                        <Route index element={<Navigate replace to='cities' />} />
                         <Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
                         <Route path="cities/:id" element={<City />} />
                         <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading} />} />
