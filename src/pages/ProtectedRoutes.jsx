89d8c0c05bb95ae5c1a5a45e954e34fc53f15d14
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-08-28 20:45:52 +0330

    add routes protector

---
+++ src/pages/ProtectedRoutes.jsx
@@ -0,0 +1,26 @@
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext"
+import { useEffect } from "react";
+
+function ProtectedRoutes({ children }) {
+
+    const { isAuthenticated } = useAuth();
+    const navigate = useNavigate()
+
+    useEffect(function () {
+        if (!isAuthenticated) navigate('/')
+    }, [isAuthenticated, navigate]);
+
+    return children
+}
+
+export default ProtectedRoutes
+
+
+
+
+
+
+
+
+
