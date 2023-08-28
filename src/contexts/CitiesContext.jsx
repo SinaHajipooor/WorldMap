e78f956bd90d523aee85637354a147ed7fd66f85
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-08-28 10:41:42 +0330

    fetch the city details

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,17 +1,20 @@
 import { createContext, useContext, useEffect, useState } from "react";
 // initialize the context
 const CitiesContext = createContext();
+// base url 
+const BASE_URL = 'http://localhost:9000';
 // states and logic of context
 function CitiesProvider({ children }) {
     // states
     const [cities, setCities] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+    const [currentCity, setCurrentCity] = useState({});
     // fetch cities list 
     useEffect(function () {
         async function fetchCities() {
             try {
                 setIsLoading(true);
-                const response = await fetch('http://localhost:9000/cities');
+                const response = await fetch(`${BASE_URL}/cities`);
                 const data = await response.json();
                 setCities(data);
             } catch (error) {
@@ -23,8 +26,21 @@ function CitiesProvider({ children }) {
         }
         fetchCities();
     }, []);
+    // fetch the current city data 
+    async function getCity(id) {
+        try {
+            setIsLoading(true);
+            const response = await fetch(`${BASE_URL}/cities/${id}`);
+            const data = await response.json();
+            setCurrentCity(data);
+        } catch (error) {
+            alert('failed to fetch city details')
+        } finally {
+            setIsLoading(false);
+        }
+    }
     // assign the values 
-    return <CitiesContext.Provider value={{ cities, isLoading }}>{children}</CitiesContext.Provider>
+    return <CitiesContext.Provider value={{ cities, isLoading, currentCity, getCity }}>{children}</CitiesContext.Provider>
 }
 // custom hook to use the cities context every where we need 
 function useCities() {
@@ -33,6 +49,8 @@ function useCities() {
     return context;
 }
 export { CitiesProvider, useCities }
+
+
 
 
 
