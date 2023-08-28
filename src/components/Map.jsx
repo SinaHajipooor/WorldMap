324e42674248b9b98e2c9de984c68520118e4c4a
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-08-28 17:59:52 +0330

    setting map positions with geolocation

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,6 +3,9 @@ import styles from './Map.module.css'
 import { MapContainer, TileLayer, Marker, Popup, useMap, useMapEvents } from 'react-leaflet';
 import { useEffect, useState } from 'react';
 import { useCities } from '../contexts/CitiesContext';
+import { useGeolocation } from '../hooks/useGeolocation';
+import Button from '../components/Button'
+
 
 function Map() {
     const [mapPosition, setMapPosition] = useState([40, 0])
@@ -15,15 +18,19 @@ function Map() {
     const mapLng = searchparams.get('lng');
     // get the navigate method
     const navigate = useNavigate();
+    const { isLoading: isLoadingPosition, position: geolocationPosition, getPosition } = useGeolocation()
 
     useEffect(function () {
         if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
     }, [mapLat, mapLng])
 
-
+    useEffect(function () {
+        if (geolocationPosition) setMapPosition([geolocationPosition.lat, geolocationPosition.lng])
+    }, [geolocationPosition])
 
     return (
         <div className={styles.mapContainer} onClick={() => navigate('form')}>
+            {!geolocationPosition && <Button type='position' onClick={getPosition}>{isLoadingPosition ? 'Loading...' : 'use your positon'}</Button>}
             <MapContainer center={mapPosition} zoom={6} scrollWheelZoom={true} className={styles.map}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
